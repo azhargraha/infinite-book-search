7d02803910fa27cc0300d68952a16443274cf491
Azhar Nugraha <azhargraha14@gmail.com>
2021-07-29 16:08:12 +0700

    mapping book data into state

--- src/App.js
+++ src/App.js
@@ -1,20 +1,62 @@
 import React, { useEffect, useState, useRef } from 'react';
 import './App.scss';
-import axios from 'axios';
+import axios, { CancelToken } from 'axios';
 
 export default function App() {
   const [query, setQuery] = useState('');
   const [pageNumber, setPageNumber] = useState(1);
+  const [books, setBooks] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [isError, setIsError] = useState(false);
+  const [hasMore, setHasMore] = useState(false);
 
   useEffect(() => {
-    axios({
-      method: 'GET',
-      url: 'http://openlibrary.org/search.json',
-      params: {
-        q: query,
-        page: pageNumber
-      }
-    }).then(console.log)
+    setBooks([]);
+  }, [query]);
+
+  useEffect(() => {   
+    let cancel;
+
+    if (query === '') {
+      setBooks([]);
+      setIsLoading(false);
+    } else {
+      setIsLoading(true);
+      setIsError(false);
+
+      axios({
+        method: 'GET',
+        url: 'http://openlibrary.org/search.json',
+        params: {
+          q: query,
+          page: pageNumber
+        },
+        cancelToken: new CancelToken(token => cancel = token)
+      }).then(res => {
+        setBooks(prevBooks => {
+          const appendNewBooks = [...res.data.docs.map(book => {
+            return {
+              title: book.title,
+              author: book.author_name
+            }
+          })];
+          return appendNewBooks;
+          // Set: prevent duplicate data 
+          // return [...new Set(appendNewBooks)];
+        })
+        console.log(res.data.docs)
+        setHasMore(res.data.docs.length > 0)
+        setIsLoading(false);
+      })
+        .catch(e => {
+          if (axios.isCancel(e)) return;
+          setIsError(true);
+        });
+    }
+
+    return () => {
+      cancel && cancel();
+    };
   }, [query, pageNumber]);
 
   return (
@@ -23,7 +65,27 @@ export default function App() {
         queryState={[query, setQuery]} 
         pageNumberState={[pageNumber, setPageNumber]}
       />
-      <div className="content-container"></div>
+      {/* {books.map(book => {
+        return (
+          <div>
+            <h4>{book.title}</h4>
+            <p>{book.author}</p>
+          </div>
+        )
+      })} */}
+      {isLoading && <p>loading jing...</p>}
+      {isError && <p>error jing...</p>}
+      <div className="content-container">
+        {books.map(book => {
+          return (
+            <div>
+              <h4>{book.title}</h4>
+              <p>{book.author}</p>
+            </div>
+          )
+        })}
+      </div>
+      {}
     </div>
   );
 };
@@ -33,6 +95,7 @@ const Form = ({ queryState, pageNumberState }) => {
   const [pageNumber, setPageNumber] = pageNumberState;
 
   const handleSearch = (e) => {
+    e.preventDefault();
     setQuery(e.target.value);
     setPageNumber(1)
   };
@@ -42,14 +105,14 @@ const Form = ({ queryState, pageNumberState }) => {
   }
 
   return (
-    <form className="Form">
-      <input type="text" autoCorrect="false" autoComplete="false" spellCheck="false" onChange={handleSearch} value={query}/>
+    <div className="Form">
+      <input type="text" placeholder="Search book title" autoCorrect="false" autoComplete="false" spellCheck="false" onChange={handleSearch} value={query}/>
       <button className="del-icon" onClick={handleDelete}>
         <svg xmlns="http://www.w3.org/2000/svg" width="100%" height="100%" viewBox="0 0 24 24">
           <line x1="19" y1="5" x2="5" y2="19" strokeMiterlimit="10"/>
           <line x1="5" y1="5" x2="19" y2="19" strokeMiterlimit="10"/>
         </svg>
       </button>
-    </form>
+    </div>
   )
 };
